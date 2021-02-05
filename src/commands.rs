8fb353ba227486b944f24ae6e8d5e5a452771a95
Ammon Taylor <ammonx9@gmail.com>
2021-02-04 22:44:36 -0700

    added spinner

--- src/commands.rs
+++ src/commands.rs
@@ -1,13 +1,11 @@
-use cmd_lib::run_cmd;
+use std::process::Command;
 
-pub fn find_file(file: &str) {
 
-    if run_cmd! {
-        pwd
-        cd ../
-        pwd
-    }.is_err() {
-        println!("file: {} could not be found.", file);
-    }
+pub fn clone_repo(name: &str) {
+	let mut dir = String::from(".gee/tmp/");
+	dir.push_str(name);
+	Command::new("git")
+		.args(&["clone", name, &dir])
+		.output()
+		.expect("git clone failed");
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,30 @@
+use spinners::{Spinner, Spinners};
+use std::env::set_current_dir;
+use std::path::Path;
 mod commands;
 
+fn test_stuff(all: bool) {
+    if all {
+        set_home_dir();
+        commands::clone_repo("hello");
+    }
+}
+
+fn set_home_dir() {
+    let root = Path::new("/Users/ammont");
+    assert!(set_current_dir(&root).is_ok());
+}
+
+fn run_command(loading_msg: &str) {
+    let sp = Spinner::new(Spinners::Dots, loading_msg.into());
+    loop{};
+    commands::clone_repo("hello");
+    sp.stop();
+}
 
 fn main() {
-    commands::find_file("~/.vimrc");
+    test_stuff(false);
+    set_home_dir();
+    run_command("loading");
+    print!(" ");
 }
