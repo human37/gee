177ed41425544272db5704a5111c572330cd7d8c
Ammon Taylor <ammonx9@gmail.com>
2021-02-06 16:02:30 -0700

    now pipes stdout correctly

--- src/commands.rs
+++ src/commands.rs
@@ -1,11 +1,30 @@
-use std::process::Command;
-
+use json;
+use std::process::{Command, Stdio};
+use std::io::prelude::*;
 
 pub fn clone_repo(name: &str) {
 	let mut dir = String::from(".gee/tmp/");
 	dir.push_str(name);
-	Command::new("git")
-		.args(&["clone", name, &dir])
-		.output()
-		.expect("git clone failed");
+	let mut output = String::new();
+	let process = match Command::new("git")
+		.args(&["clone", "--progress", name, &dir])
+		.stderr(Stdio::piped())
+		.stdout(Stdio::piped())
+		.spawn() {
+			Err(why) => panic!("error with output: {}", why),
+			Ok(process) => process 
+	};
+
+	match process.stderr.unwrap().read_to_string(&mut output) {
+		Err(why) => panic!("error: {}", why),
+        Ok(_) => print!("\noutput: {}", output),
+	}
+
+	write_to_json(name);
+}
+
+fn write_to_json(name: &str) {
+	let mut data = json::JsonValue::new_object();
+	data["repositories"] = name.into();
+	println!("{}", data);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,6 @@
 use spinners::{Spinner, Spinners};
-use std::env::set_current_dir;
-use std::path::Path;
+use std::{env::set_current_dir, fs::read_to_string, path::Path, thread::sleep, time::Duration};
 mod commands;
-
-fn test_stuff(all: bool) {
-    if all {
-        set_home_dir();
-        commands::clone_repo("hello");
-    }
-}
 
 fn set_home_dir() {
     let root = Path::new("/Users/ammont");
@@ -17,14 +9,19 @@ fn set_home_dir() {
 
 fn run_command(loading_msg: &str) {
     let sp = Spinner::new(Spinners::Dots, loading_msg.into());
-    loop{};
-    commands::clone_repo("hello");
+    sleep(Duration::from_secs(1));
+    commands::clone_repo("git@github.com:human37/stockbot.git");
     sp.stop();
 }
 
+fn read_configurations() {
+    println!("in file {}", ".geerc");
+    let contents = read_to_string(".geerc").expect("something went wrong reading the file");
+    println!("with text:\n{}", contents);
+}
+
 fn main() {
-    test_stuff(false);
     set_home_dir();
-    run_command("loading");
-    print!(" ");
+    read_configurations();
+    run_command("cloning repostiory...");
 }
