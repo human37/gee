5664f9b3c200487156e51a1aacfe1400c650a4c8
Ammon Taylor <ammonx9@gmail.com>
2021-03-04 23:32:49 -0700

    basics of cli working

--- src/gee/utils.rs
+++ src/gee/utils.rs
@@ -1,5 +1,6 @@
-use std::{fs::File, io::prelude::*, path::Path, process::Command, process::Stdio};
-
+use spinners::{Spinner, Spinners};
+use std::{fs::File, io::prelude::*, path::Path, process::Command, process::Stdio, env::set_current_dir};
+use super::Gee;
 /// PARAMS: path = a path to the file.
 /// it will return true if the file exists,
 /// and false if the file does not exists.
@@ -60,4 +61,31 @@ pub fn remove_repo(name: &str) -> std::io::Result<()> {
         log_error(process).expect("logging error failed.");
     }
     Ok(())
+}
+
+/// PARAMS: none. 
+/// sets the working directory to the user's home directory.
+pub fn set_home_dir() {
+    let root = Path::new("/Users/ammont");
+    assert!(set_current_dir(&root).is_ok());
+}
+
+/// calls the 'clone_repo' function, and also
+/// shows a loading spinner while the command is running
+pub fn test_run_command(loading_msg: &str) {
+    let sp = Spinner::new(Spinners::Dots, loading_msg.into());
+
+    let mut g = Gee::new();
+    g.init();
+    g.clone_repo("git@github.com:human37/gee.git")
+        .expect("error");
+    g.clone_repo("git@github.com:human37/stockbot.git")
+        .expect("error");
+    g.clone_repo("git@github.com:human37/ppm_editor.git")
+        .expect("error");
+    g.clone_repo("git@github.com:human37/lyrics_microservice.git")
+        .expect("error");
+    g.clone_repo("git@github.com:evad1n/F-sharp.git")
+        .expect("error");
+    sp.stop();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,22 @@
-use spinners::{Spinner, Spinners};
-use std::{env::set_current_dir, path::Path};
 mod gee;
-/// sets the working directory to the user's home directory
-fn set_home_dir() {
-    let root = Path::new("/Users/ammont");
-    assert!(set_current_dir(&root).is_ok());
-}
+use clap::{load_yaml, App};
 
-/// calls the 'clone_repo' function, and also
-/// shows a loading spinner while the command is running
-fn run_command(loading_msg: &str) {
-    let sp = Spinner::new(Spinners::Dots, loading_msg.into());
 
-    let mut g = gee::Gee::new();
-    g.init();
-    g.clone_repo("git@github.com:human37/gee.git")
-        .expect("error");
-    g.clone_repo("git@github.com:human37/stockbot.git")
-        .expect("error");
-    g.clone_repo("git@github.com:human37/ppm_editor.git")
-        .expect("error");
-    g.clone_repo("git@github.com:human37/lyrics_microservice.git")
-        .expect("error");
-    g.clone_repo("git@github.com:evad1n/F-sharp.git")
-        .expect("error");
-    sp.stop();
-}
-
-/// entrypoint to gee
 fn main() {
-    set_home_dir();
-    run_command("loading...  ");
+
+    let yaml = load_yaml!("cli.yaml");
+    let m = App::from(yaml).get_matches();
+
+    if let Some(clone) = m.value_of("clone") {
+        match clone {
+            "vi" => println!("You are using vi"),
+            "emacs" => println!("You are using emacs..."),
+            _ => unreachable!(),
+        }
+    } else {
+        println!("--clone <CLONE> wasn't used...")
+    }
+
+    gee::utils::set_home_dir();
+    gee::utils::test_run_command("loading...  ");
 }
